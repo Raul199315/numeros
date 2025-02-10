4e450d356693c23020ac8df1daebdf954ff3bf6a
Raul199315 <102283554+Raul199315@users.noreply.github.com>
2025-02-09 22:34:31 -0500

    numeroSecreto v0.7
    se agrega funcion para selecionar dificultad entre 5 10 o 20

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let numeroSecreto = 0;
-let numeroMaximo = 10;
+let numeroMaximo = 0;
 let intento = 0;
-let lista = [];
-console.log(numeroSecreto);
 
+//let lista = [];
+//console.log(numeroSecreto);
 
+/*
 function generarNumero(){
     let numero = Math.floor(Math.random()*numeroMaximo)+1;
     console.log(numero)
@@ -23,22 +24,33 @@ function generarNumero(){
 }
     
 }
+*/
 
+function generarNumero(){
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    numeroSecreto = numeroGenerado
+    console.log(numeroSecreto)
+    return numeroGenerado;
+}
 
 function validaIntento(){
     let numeroUsuario = document.querySelector('input').value;
     if( numeroUsuario == numeroSecreto){
+        intento++
         textos('h1',`LO ADIVINASTE`);
-        textos('p',`Adivinaste el numero secreto lo lograste en ${intento}`);
+        textos('p',`Adivinaste el numero secreto lo lograste en ${intento} intentos`);
     }else{
         if(numeroUsuario > numeroSecreto){
             textos('h1',`FALLASTE`);
             textos('p',`el numero es menor`);
+            intento++
         }else{
             textos('h1',`FALLASTE`);
             textos('p',`el numero es mayor`);
+            intento++
         }
     }
+    
 }
 
 
@@ -58,9 +70,12 @@ generarNumero();
 function dificultad(){
     let nivel = document.querySelector('select').value;
     numeroMaximo = nivel
-    console.log(numeroMaximo)
     textos('p',`para jugar ingresa un numero entre 1 y ${numeroMaximo}`);
+    document.getElementById('botonIntento').removeAttribute('disabled');
+    generarNumero()
 }
-generarNumero();
 
+
+generarNumero();
 textos('h1',`Bienvenido al Juego Adivina el numero Secreto`);
+textos('p','para jugar selecina una dificultad')
